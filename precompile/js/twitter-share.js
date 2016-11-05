856204fbb8ec145fbeef077f29c5d95f69cf50a5
Joshua Briley <josh@thebrileys.com>
2016-11-04 22:01:17 -0400

    Add twitter share.

---
+++ precompile/js/twitter-share.js
@@ -0,0 +1,30 @@
+var blockQuote = document.querySelectorAll('blockquote')[0];
+var blockQuoteText = blockQuote.querySelectorAll('p')[0].textContent;
+var twitterButton = document.querySelector(".share-button");
+var twitterHandle = "MjesticMonolith";
+var twitterStringExtension = "via @" + twitterHandle;
+
+function truncateString() {
+  var trimLength = 140 - twitterStringExtension.length;
+  var length = 6;
+  var trimmedString = string;
+}
+
+function setTwitterLink(){
+  //console.log(blockQuoteText);
+  // https://dev.twitter.com/web/tweet-button/web-intent
+  var currentURL = window.location.href;
+  var currentURLStringLength = currentURL.length;
+  var quoteLength = blockQuoteText.length;
+  var truncationLength = 140 - (currentURLStringLength + twitterStringExtension.length) - 3;
+  var link = "https://twitter.com/intent/tweet?via=MjesticMonolith&url=" + currentURL + "&text=";
+  var truncatedQuote = blockQuoteText.substring(0, truncationLength);
+  console.log(currentURL);
+  if(quoteLength < truncationLength){
+    twitterButton.setAttribute('href', link + blockQuoteText);
+  } else {
+    twitterButton.setAttribute('href', link + truncatedQuote + "…");
+  }
+}
+
+twitterButton.addEventListener("click", setTwitterLink);
