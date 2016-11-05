d32217619c563013373dfaacd586153d937b7702
Joshua Briley <josh@thebrileys.com>
2016-11-04 22:34:09 -0400

    Twitter share refactor.

--- precompile/js/twitter-share.js
+++ precompile/js/twitter-share.js
@@ -1,22 +1,20 @@
-function setTwitterLink(){
-  var blockQuote = document.querySelectorAll('blockquote')[0];
-  var blockQuoteText = blockQuote.querySelectorAll('p')[0].textContent;
-  var twitterButton = document.querySelector(".share-button");
-  var twitterHandle = "MjesticMonolith";
-  var twitterStringExtension = "via @" + twitterHandle;
+// https://dev.twitter.com/web/tweet-button/web-intent
 
-  // https://dev.twitter.com/web/tweet-button/web-intent
-  var currentURL = window.location.href;
-  var currentURLStringLength = currentURL.length;
-  var quoteLength = blockQuoteText.length;
-  var truncationLength = 140 - (currentURLStringLength + twitterStringExtension.length) - 3;
-  var link = "https://twitter.com/intent/tweet?via=MjesticMonolith&url=" + currentURL + "&text=";
-  var truncatedQuote = blockQuoteText.substring(0, truncationLength);
-  if(quoteLength < truncationLength){
-    twitterButton.setAttribute('href', link + blockQuoteText);
+function setTwitterLink(){
+  var blockQuote = document.querySelectorAll('blockquote')[0].querySelectorAll('p')[0].textContent,
+      currentURL = window.location.href,
+      handle = "MjesticMonolith",
+      handleExtension = "via @" + twitterHandle,
+      link = "https://twitter.com/intent/tweet?via=" + handle + "&url=" + currentURL + "&text=",
+      shareButton = document.querySelector(".share-button"),
+      truncationLength = 140 - (currentURLString.length + handleExtension.length) - 3,
+      truncatedQuote = blockQuote.substring(0, truncationLength);
+
+  if(blockQuote.length < truncationLength){
+    shareButton.setAttribute('href', link + blockQuote);
   } else {
-    twitterButton.setAttribute('href', link + truncatedQuote + "…");
+    shareButton.setAttribute('href', link + truncatedQuote + "…");
   }
 }
 
-twitterButton.addEventListener("click", setTwitterLink);
+shareButton.addEventListener("click", setTwitterLink);
